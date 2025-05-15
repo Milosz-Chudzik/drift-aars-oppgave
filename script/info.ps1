df1f523a790854a8db549a41e1ca0aa5bfcedede
Milosz <miloszchudzik07@gmail.com>
2025-05-15 13:01:11 +0200

    legger til ren kode

---
+++ script/info.ps1
@@ -0,0 +1,51 @@
+﻿# --- Nettverksinnstillinger ---
+$env:HostIP = (
+    Get-NetIPConfiguration |
+    Where-Object {
+        $_.IPv4DefaultGateway -ne $null -and
+        $_.NetAdapter.Status -ne "Disconnected"
+    }
+).IPv4Address.IPAddress
+$prefixLength = 24 # Tilsvarer nettverksmaske 255.255.255.0
+$gateway = "192.168.1.1"
+# DNS-server(e). Etter AD/DNS-installasjon bør serveren peke til seg selv (127.0.0.1) og evt. en annen DNS.
+$dnsServers = "127.0.0.1" # Kan også være en liste, f.eks. @("127.0.0.1", "192.168.1.1")
+
+# --- Active Directory-innstillinger ---
+$domainName = "ove-server.dm" # Du kan bare bruke karakterer fra a-z, tall og bindestrek. til slutt må du ha .dm/com/no eller organisasjons domenet ditt.
+$netbiosName = "OVE-SERVER"      # NetBIOS-navn (vanligvis første del av $domainName, maks 15 tegn).
+# Du vil bli bedt om et SafeModeAdministratorPassword under AD-installasjonen.
+# Alternativt, definer det her (FJERN KOMMENTAREN og sett et STERKT passord):
+# $safeModePassword = ConvertTo-SecureString "DittTryggePassordHer!" -AsPlainText -Force
+
+# --- Roller som skal installeres ---
+# Legg til eller fjern roller etter behov
+$windowsFeatures = @(
+    "AD-Domain-Services",
+    "DNS",
+    "DHCP",
+    "Hyper-V",
+    "FS-FileServer"
+)
+
+# --- DHCP-innstillinger (hvis DHCP-rollen installeres) ---
+$dhcpScopeName = "DefaultScope"
+$dhcpStartRange = "192.168.1.100"
+$dhcpEndRange = "192.168.1.200"
+$dhcpSubnetMask = "255.255.255.0"
+# $dhcpRouterOption = $gateway # Angi gateway for klienter
+# $dhcpDnsServerOption = $ipAddress # Angi DNS-server(e) for klienter (denne serveren)
+
+# --- OU-navn ---
+$ouComputersBase = "Maskiner" # Hoved-OU for datamaskiner
+$ouUsersBase = "Brukere"     # Hoved-OU for brukere
+$ouITComputers = "IT-Maskiner" # Spesifikk OU for IT-datamaskiner (brukes for redircmp)
+$ouGroups = "Grupper"        # OU for grupper
+$ouServers = "Servere"       # OU for servere
+
+# --- Testbruker ---
+$testUserName = "Test Bruker"
+$testUserSamAccount = "test.bruker"
+# Passord for testbrukeren (BYTT TIL ET STERKT PASSORD!)
+$testUserPassword = ConvertTo-SecureString "P@ssw0rd123!" -AsPlainText -Force
+$testUserChangePasswordAtLogon = $false # Sett til $true for å tvinge passordbytte ved første pålogging
---
+++ script/kommandoer.ps1
@@ -0,0 +1,97 @@
+﻿Get-NetAdapter -Name $interfaceAlias | New-NetIPAddress -IPAddress $ipAddress -PrefixLength $prefixLength -DefaultGateway $gateway
+
+Get-NetAdapter -Name $interfaceAlias | Set-DnsClientServerAddress -ServerAddresses $dnsServers
+
+Install-WindowsFeature -Name $windowsFeatures -IncludeAllSubFeature -IncludeManagementTools
+
+Get-WindowsFeature -Name $windowsFeatures
+
+Restart-Computer -Force
+
+$installADDSForestParams = @{
+    DomainName = $domainName
+    DomainNetbiosName = $netbiosName
+    InstallDns = $true
+    CreateDnsDelegation = $false
+    DatabasePath = "C:\Windows\NTDS"
+    LogPath = "C:\Windows\NTDS"
+    SysvolPath = "C:\Windows\SYSVOL"
+    Force = $true # Bruk Force med forsiktighet, bekrefter automatiske omstarter etc.
+}
+if ($PSBoundParameters.ContainsKey('safeModePassword')) {
+    $installADDSForestParams.Add("SafeModeAdministratorPassword", $safeModePassword)
+}
+
+Install-ADDSForest @installADDSForestParams
+
+
+if (Get-WindowsFeature -Name DHCP | Where-Object { $_.Installed }) {
+
+    # Hent serverens FQDN (viktig for autorisasjon)
+    $serverFQDN = (Get-ADDomainController -Discover -Service PrimaryDC).HostName
+    $serverIPAddress = $env:HostIP # Bruker IP-adressen satt tidligere
+
+    # Autoriser DHCP-serveren i Active Directory
+    Add-DhcpServerInDC -DnsName $serverFQDN -IPAddress $serverIPAddress
+
+    # Opprett et nytt DHCP-scope
+    Add-DhcpServerv4Scope -Name $dhcpScopeName `
+        -StartRange $dhcpStartRange `
+        -EndRange $dhcpEndRange `
+        -SubnetMask $dhcpSubnetMask `
+        -State Active
+
+    # Sett DHCP-opsjoner (f.eks. Ruter/Gateway og DNS-servere)
+    $dhcpOptions = @{}
+    if ($PSBoundParameters.ContainsKey('dhcpRouterOption')) { $dhcpOptions.Add("003", $dhcpRouterOption) }
+    if ($PSBoundParameters.ContainsKey('dhcpDnsServerOption')) { $dhcpOptions.Add("006", $dhcpDnsServerOption) }
+    if ($dhcpOptions.Count -gt 0) {
+        Set-DhcpServerv4OptionValue -ScopeId $ipAddress -OptionId $dhcpOptions.Keys -Value $dhcpOptions.Values
+    }
+
+
+    # Start DHCP-tjenesten på nytt for å aktivere endringer
+    Restart-Service DhcpServer
+
+    
+} else {
+    Install-WindowsFeature -NAME DHCP -IncludeManagementTools
+    Write-Host "DHCP-rollen er ikke installert. installerer dns." -ForegroundColor Yellow
+}
+
+$domainPath = "DC=ove-server, DC=dm"
+
+New-ADOrganizationalUnit -Name $ouComputersBase -Path $domainPath -Description "Organisatorisk enhet for datamaskiner"
+New-ADOrganizationalUnit -Name $ouUsersBase -Path $domainPath -Description "Organisatorisk enhet for brukere"
+New-ADOrganizationalUnit -Name $ouGroups -Path $domainPath -Description "Organisatorisk enhet for grupper"
+New-ADOrganizationalUnit -Name $ouServers -Path $domainPath -Description "Organisatorisk enhet for servere"
+
+#setter standard gruppe for brukere og maskiner
+$computerRedirectPath = "OU=$ouITComputers,OU=$ouComputersBase,$domainPath"
+Write-Host "Omdirigerer nye datamaskiner til: $computerRedirectPath"
+redircmp $computerRedirectPath
+
+# Omdiriger 'Users'-containeren til OU for Brukere
+$userRedirectPath = "OU=$ouUsersBase,$domainPath"
+redirusr $userRedirectPath
+
+$testUserUPN = "$testUserSamAccount@$domainName"
+# Definer stien til bruker-OUen
+$userOUFullPath = "OU=$ouUsersBase,$domainPath"
+
+# Opprett brukeren
+New-ADUser -Name $testUserName `
+    -SamAccountName $testUserSamAccount `
+    -UserPrincipalName $testUserUPN `
+    -Path $userOUFullPath `
+    -AccountPassword $testUserPassword `
+    -Enabled $true `
+    -ChangePasswordAtLogon:$testUserChangePasswordAtLogon
+
+
+$createdUser = Get-ADUser -Filter "SamAccountName -eq '$testUserSamAccount'"
+if ($createdUser) {
+    Write-Host "Testbruker '$testUserName' opprettet i OU '$($createdUser.DistinguishedName)'." -ForegroundColor Green
+} else {
+    Write-Host "Kunne ikke verifisere opprettelsen av testbruker '$testUserName'." -ForegroundColor Red
+}
